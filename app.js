e911093c258ebf5b892a9a88b9c52175417b92e8
Santiago Ferreira <san650@gmail.com>
2017-08-12 11:45:56 -0300

    mustache

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var app = require('./api');
+var path = require("path");
+var mustache = require("mustache-express");
+var app = require("./app/index");
+
+app.engine("html", mustache());
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "app", "views"));
 
 app.listen(3000, function() {
   console.log("http://localhost:3000");
