16018d567e43cb67f7f2e8cc35dcbc9d4e3dd48f
Santiago Ferreira <san650@gmail.com>
2017-08-12 11:26:30 -0300

    WIP

--- api/index.js
+++ api/index.js
@@ -1,8 +1,29 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+var db = require("./db");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/ping", function(req, res) {
   res.send("pong");
+});
+
+app.get("/users/:userId", function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  res.json(db.get(req.params.userId));
+});
+
+app.post("/users", function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  var user = db.add({
+    name: req.body.name
+  });
+
+  res.json(user);
 });
 
 module.exports = app;
--- test/api-test.js
+++ test/api-test.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
 var request = require("supertest");
 var api = require("../api");
+var db = require("../api/db");
 
 describe("GET /ping", function() {
 
@@ -10,6 +11,44 @@ describe("GET /ping", function() {
       .expect(200)
       .then(function(response) {
         assert.equal(response.text, "pong");
+      });
+  });
+
+});
+
+describe("GET /users/:userId", function() {
+
+  it("returns user data for John", function() {
+    db.add({
+      id: 1,
+      name: "John Doe"
+    });
+
+    return request(api)
+      .get("/users/1")
+      .expect(200)
+      .then(function(response) {
+        assert.equal(response.body.id, 1);
+        assert.equal(response.body.name, "John Doe");
+      });
+  });
+
+});
+
+describe("POST /users", function() {
+
+  it("returns 200 if the user is saved correctly", function() {
+    return request(api)
+      .post("/users")
+      .send({name: "Paul Roe"})
+      .expect(200)
+      .then(function(response) {
+        var userId = response.body.id;
+        var user = db.get(userId);
+
+        assert.ok(user);
+        assert.equal(user.id, userId);
+        assert.equal(user.name, "Paul Roe");
       });
   });
 
