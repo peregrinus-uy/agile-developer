26fd5357526c0c3d054b6ab25e651d9dd9dab688
Santiago Ferreira <san650@gmail.com>
2017-11-17 10:44:36 -0300

    Return error exit code when a test fails

--- test/run.js
+++ test/run.js
@@ -4,7 +4,13 @@ const app = require('../app');
 const server = app.listen(3001, () => {
   console.info('Running tests on port 3001...');
 
-  return cypress.run().then(() => {
-    server.close();
+  return cypress.run().then((results) => {
+    server.close(() => {
+      // We need to end the process with an error code if there are failures.
+      // This is used by the CI server to mark the build as failed.
+      if (results.failures) {
+        process.exit(1);
+      }
+    });
   });
 });
