8ade77bc847f4719acb67a14aee9b89f770ffac6
Matias Delgado <matiasdel@gmail.com>
2017-11-18 16:35:09 -0300

    Gauges

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,37 @@
+const issueStore = require('../datastore/issue.store');
+
 function loadRoutes(app) {
-  app.get('/', function(req, res) {
-    res.render('index', { currentDate: new Date().toLocaleDateString() });
-  });
+	app.get('/', function(req, res) {
+		const issues = issueStore.getAll();
+		const criticalIssues = issues.filter(issue => issue.severity === 'Critical');
+		const highIssues = issues.filter(issue => issue.severity === 'High');
+		const mediumIssues = issues.filter(issue => issue.severity === 'Medium');
 
-  app.post('/session', function(req, res) {
-    res.send('12345');
-  });
+		res.render('index', {
+			criticalPercent: criticalIssues.length / issues.length,
+			highPercent: highIssues.length / issues.length,
+			mediumPercent: mediumIssues.length / issues.length
+		});
+	});
 
-  app.use('/issues', require('./issue'));
+	app.post('/session', function(req, res) {
+		res.send('12345');
+	});
 
-  // FIXME: Test to enable only on test.
-  // if (process.env.NODE_ENV === 'test') {
-  app.post('/test/reset', function(req, res) {
-    require('../datastore/issue.store').reset();
-    res.send('OK');
-  });
+	app.use('/issues', require('./issue'));
 
-  app.post('/test/seed', function(req, res) {
-    console.log(req.body);
-    require('../datastore/issue.store').add(req.body);
-    res.send('OK');
-  });
+	// FIXME: Test to enable only on test.
+	// if (process.env.NODE_ENV === 'test') {
+	app.post('/test/reset', function(req, res) {
+		issueStore.reset();
+		res.send('OK');
+	});
+
+	app.post('/test/seed', function(req, res) {
+		console.log(req.body);
+		issueStore.add(req.body);
+		res.send('OK');
+	});
 }
 
 module.exports = loadRoutes;
