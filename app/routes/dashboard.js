d2ae8c9798df958b728accfd06c12295f7165756
Matias Delgado <matiasdel@gmail.com>
2017-11-25 21:44:21 -0300

    Remove gauges feature

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,23 +1,8 @@
 const express = require('express');
-const issuesDB = require('../models/issue.store');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  const issues = issuesDB.getAllOpen();
-
-  const criticalIssues = issues.filter(issue => issue.severity === 'Critical');
-  const highIssues = issues.filter(issue => issue.severity === 'High');
-  const mediumIssues = issues.filter(issue => issue.severity === 'Medium');
-  const closed = issues.filter(issue => issue.status === 'closed');
-  const openCount = issues.filter(issue => issue.status === 'open');
-
-  res.render('dashboard', {
-    criticalPercent: criticalIssues.length / issues.length,
-    highPercent: highIssues.length / issues.length,
-    mediumPercent: mediumIssues.length / issues.length,
-    closedPercent: closed.length / issues.length,
-    openCount: openCount.length
-  });
+  res.render('dashboard', {});
 });
 
 module.exports = router;
