b15a3eb8dce3d967f4cde7cc9e084ca20b5e34f3
Santiago Ferreira <san650@gmail.com>
2017-11-28 17:03:26 -0300

    Fix initial test

--- test/integration/user_adds_issue_spec.js
+++ test/integration/user_adds_issue_spec.js
@@ -1,19 +1,32 @@
-describe('User adds a new issue', function() {
+describe('New issues', function() {
+
   it('User adds a new issue', function() {
     cy.visit('/issues');
-    cy.dataTest('add-issue').click();
 
-    cy.field('title').type('Blue screen in Windows Vista');
-    cy.field('estimation').select('13');
-    // cy.field('severity').select('High');
-    cy.field('description').type('When I try to play solitaire in Windows, it crashes with a blue screen');
-    cy.get('[type="submit"]').click();
+    cy.get('[data-test-add-issue]')
+      .click();
 
-    cy.contains('Blue screen in Windows Vista').click();
+    cy.get('[name="issue[title]"]')
+      .type('Blue screen in Windows Vista');
+
+    cy.get('[name="issue[estimation]"]')
+      .select('13');
+
+    // cy.get('[name="issue[severity]"]').select('High');
+
+    cy.get('[name="issue[description]"]')
+      .type('When I try to play solitaire in Windows, it crashes with a blue screen');
+
+    cy.get('[type="submit"]')
+      .click();
+
+    cy.contains('Blue screen in Windows Vista')
+      .click();
 
     cy.contains('Estimation').should('exist');
     cy.contains('13').should('exist');
     // cy.contains('Severity').should('exist');
     // cy.contains('High').should('exist');
   });
+
 });
