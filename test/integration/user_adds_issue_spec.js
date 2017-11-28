cca58b3a44f1984532bb42fcf6a509771b8ec2f4
Matias Delgado <matiasdel@gmail.com>
2017-11-27 21:55:46 -0300

    Fix initial tests

--- test/integration/user_adds_issue_spec.js
+++ test/integration/user_adds_issue_spec.js
@@ -1,14 +1,19 @@
 describe('User adds a new issue', function() {
   it('User adds a new issue', function() {
     cy.visit('/issues');
-    cy.get('[data-test-add-issue]').click();
+    cy.dataTest('add-issue').click();
 
-    cy.get('[name="issue[title]"]').type('Blue screen in Windows Vista');
-    cy.get('[name="issue[severity]"]').select('High');
-    cy.get('[name="issue[estimation]"]').select('1');
-    cy.get('[name="issue[description]"').type('When I try to play solitaire in Windows, it crashes with a blue screen');
-    cy.get('button').click();
+    cy.field('title').type('Blue screen in Windows Vista');
+    cy.field('estimation').select('13');
+    // cy.field('severity').select('High');
+    cy.field('description').type('When I try to play solitaire in Windows, it crashes with a blue screen');
+    cy.get('[type="submit"]').click();
 
-    cy.contains('Blue screen in Windows Vista');
+    cy.contains('Blue screen in Windows Vista').click();
+
+    cy.contains('Estimation').should('exist');
+    cy.contains('13').should('exist');
+    // cy.contains('Severity').should('exist');
+    // cy.contains('High').should('exist');
   });
 });
